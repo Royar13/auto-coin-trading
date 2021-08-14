58e7e5a8595aa2f2fdee4d12cbb9427686b2ca4f
Roy Arad <royar13@gmail.com>
2021-08-14 17:56:26 +0300

    Finish fetching exchange rates matrix

--- contracts/AutoCoinTrader.sol
+++ contracts/AutoCoinTrader.sol
@@ -29,23 +29,4 @@ contract AutoCoinTrader {
         IUniswapV2Router02 uniswapRouter = IUniswapV2Router02(uniswapRouterAddr);
         return uniswapRouter.swapExactTokensForTokens(amountIn, amountOutMin, path, address(this), block.timestamp);
     }
-
-
-    //    function trade2(address uniswapRouterAddr, uint amountOut) public view returns (uint[] memory amounts) {
-    //        // IUniswapV2Pair pair =
-    //        //     IUniswapV2Pair(
-    //        //         UniswapV2Library.pairFor(
-    //        //             uniswapFactoryAddr,
-    //        //             "0xf3a6679b266899042276804930b3bfbaf807f15b",
-    //        //             "0xf92b8cd34853d91425f79fce8c438366443ffcd7"
-    //        //         )
-    //        //     );
-    //        IUniswapV2Router02 uniswapRouter = IUniswapV2Router02(uniswapRouterAddr);
-    //
-    //
-    //        //        address[] memory path = new address[](2);
-    //        //            path[0] = address(0xEa14a7826078Bed4Fe9F41EC322A802f169B98b9);
-    //        //        path[1] = uniswapRouter.WETH();
-    //        //        return uniswapRouter.getAmountsIn(amountOut, path);
-    //    }
 }
--- web/server/find_arbitrage.js
+++ web/server/find_arbitrage.js
@@ -1,11 +1,11 @@
-var axios = require('axios')
+const axios = require('axios')
 const Web3 = require('web3');
-const path = require("path");
-const fs = require("fs");
-const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://kovan.infura.io/ws/v3/80acc13fcbcf4fe6a380a05c3231772e"));
+const path = require('path');
+const fs = require('fs');
+const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://kovan.infura.io/ws/v3/80acc13fcbcf4fe6a380a05c3231772e'));
 
-const uniswapRouterAddr = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-const uniswapRouterJsonPath = path.resolve(process.cwd(), "build/contracts/UniswapV2Router02.json");
+const uniswapRouterAddr = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+const uniswapRouterJsonPath = path.resolve(process.cwd(), 'build/contracts/UniswapV2Router02.json');
 const uniswapRouterJson = JSON.parse(fs.readFileSync(uniswapRouterJsonPath));
 const uniswapRouterAbi = uniswapRouterJson.abi;
 
@@ -13,20 +13,17 @@ let coins = [
     {
         name: 'Etherium',
         unit: 'WETH',
-        address: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
-        value: 1
+        address: '0xd0a1e359811322d97991e03f863a0c30c2cf029c'
     },
     {
         name: 'NewSampleToken',
         unit: 'NTOK',
-        address: '0xEa14a7826078Bed4Fe9F41EC322A802f169B98b9',
-        value: 4.81939
+        address: '0xEa14a7826078Bed4Fe9F41EC322A802f169B98b9'
     },
     {
         name: 'MichalToken',
         unit: 'MST',
-        address: '0x0E9999fbe2fe4f93A9f3B2E094d5b49222754114',
-        value: 1.2
+        address: '0x0E9999fbe2fe4f93A9f3B2E094d5b49222754114'
     }
 ]
 
@@ -36,7 +33,8 @@ let cryptoExchangeMat = [
     [5, 1 / 5, 1]
 ]
 
-exchangeRateForCoins();
+createExchangeRatesMat();
+
 
 module.exports = {
     getExchangePath: function () {
@@ -60,61 +58,40 @@ function getCoin(index) {
     return coins[index];
 }
 
-function buildExchangeMat(cryptoCoins) {
-    let exchangeMat = []
-    let exchangeMatOrig = []
-
-    for (let i = 0; i < cryptoCoins.length; i++) {
-        let coin = cryptoCoins[i]
-        exchangeMatOrig[i] = []
-        exchangeMat[i] = []
-        for (let j = 0; j < cryptoCoins.length; j++) {
-            let otherCoin = cryptoCoins[j]
-            exchangeMatOrig[i][j] = otherCoin['value'] / coin['value']
-            exchangeMat[i][j] = -Math.log(otherCoin['value'] / coin['value'])
-        }
-    }
-    //console.log(coins.map(c => c.name).join(','))
-    for (let i = 0; i < cryptoCoins.length; i++) {
-        //console.log(exchangeMat[i].join(','))
-    }
-    return exchangeMat
-}
-
-async function fetchUniswapExchangeRates(tokenA, tokenB) {
+async function fetchUniswapExchangeRate(tokenA, tokenB) {
     const uniswapRouter = new web3.eth.Contract(uniswapRouterAbi, uniswapRouterAddr);
-    await uniswapRouter.methods.getAmountsOut(1.0, [tokenA, tokenB]).call()
+    let one = web3.utils.toWei('1', 'ether');
+    let amountsOut = await uniswapRouter.methods.getAmountsOut(one, [tokenA, tokenB]).call();
+    return parseFloat(web3.utils.fromWei(amountsOut[1], 'ether'));
 }
 
-async function exchangeRateForCoins() {
+async function createExchangeRatesMat() {
     let ratesMat = []
-    let rate
-    for (let i = 0; i < coins.length - 2; i++) {
-        ratesMat.push([])
-        for (let j = i + 1; j < coins.length - 1; j++) {
-            rate = await fetchUniswapExchangeRates(coins[i].address, coins[j].address)
-            ratesMat[i][j] = rate
+    //initialize 2-dimensional array
+    for (let i = 0; i < coins.length; i++) {
+        ratesMat[i] = [];
+        ratesMat[i][i] = 1;
+    }
+
+    for (let i = 0; i < coins.length - 1; i++) {
+        for (let j = i + 1; j < coins.length; j++) {
+            let rate = await fetchUniswapExchangeRate(coins[i].address, coins[j].address);
+            ratesMat[i][j] = rate;
+            ratesMat[j][i] = 1 / rate;
         }
     }
-    for (let i = 0; i < coins.length - 2; i++) {
-        for (let j = i; j < coins.length - 1; j++) {
-            if (i === j) {
-                ratesMat[i][j] = 1
-            }
-            ratesMat[j][i] = 1 / ratesMat[i][j]
-        }
-    }
-    console.log(ratesMat)
-    return ratesMat
+
+    console.log(ratesMat);
+    return ratesMat;
 }
 
 function applyLogMat(exchangeMat) {
-    exchangeMatLog = []
+    let exchangeMatLog = []
     for (let i = 0; i < exchangeMat.length; i++) {
         exchangeMatLog[i] = []
         for (let j = 0; j < exchangeMat.length; j++) {
             //todo: fix
-            exchangeMatLog[i][j] = -Math.log(1 / exchangeMat[i][j])
+            exchangeMatLog[i][j] = -Math.log(1 / exchangeMat[i][j]);
         }
     }
     return exchangeMatLog
@@ -125,7 +102,7 @@ function arbitrage(exchangeMat, source, coins) {
     let minDist = []
     let pre = []
     for (let i = 0; i < n; i++) {
-        // null means "Infinity"
+        // null signifies "Infinity"
         minDist.push(null)
         pre.push(null)
     }
@@ -161,9 +138,9 @@ function arbitrage(exchangeMat, source, coins) {
 }
 
 function getCycle(pre, source, dest, coins) {
-    var cycle = [dest, source]
-    var con = true
-    var currCoin = source
+    let cycle = [dest, source]
+    let con = true
+    let currCoin = source
     while (con) {
         cycle.push(pre[currCoin])
         currCoin = pre[currCoin]
@@ -171,7 +148,7 @@ function getCycle(pre, source, dest, coins) {
     }
     cycle.push(pre[currCoin])
 
-    if (cycle[0] == cycle[cycle.length - 1]) {
+    if (cycle[0] === cycle[cycle.length - 1]) {
         console.log(cycle.map(c => coins[c].name).join('-->'))
         return cycle
     } else {
